ecb7087f1d40aa426f98a42284c39df7285f02e5
nikich471-droid <nikich471@gmail.com>
2026-03-23 01:15:30 +0300

    boot.sh

--- boot.sh
+++ boot.sh
@@ -1,5 +1,15 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-termux-volume music 4
+termux-volume music 30
 sleep 1
 termux-media-player play ~/file.mp3
+sleep 1
+termux-volume music 30
+sleep 5
+termux-volume music 30
+sleep 3
+termux-volume music 30
+sleep 16
+termux-volume music 30
+sleep 30 
+termux-volume music 30
