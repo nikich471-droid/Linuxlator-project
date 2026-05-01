61f9eed0ce1e513369941f3e3d9c757d67602374
nikich471-droid <nikich471@gmail.com>
2026-05-01 19:29:37 +0300

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,5 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
-
+termux-wallpaper -f /data/data/com.termux/files/home/Linuxlator-project/file.jpg
 termux-volume music 30
 sleep 1
 termux-media-player play ~/file.mp3
